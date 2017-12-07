8d7a185ba6a47e58c4d53802d3e0889707674d0f
Saraswati Saud <saraswati.saud@gmail.com>
2017-12-07 20:04:52 +0545

    Create class components in ReactJS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-alert('hi')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Hello extends React.Component {
+  render() {
+    return(
+      <h1>Hello</h1>
+    );
+  }
+}
+
+class World extends React.Component {
+  render() {
+    return(
+      <p>World</p>
+    );
+  }
+}
+
+class HelloWorld extends React.Component {
+  render() {
+    return(
+      <div>
+        <Hello />
+        <World />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById('root'))
