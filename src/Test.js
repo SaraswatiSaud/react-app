256b3afb86daf09a1804422a8c55e61e65733e64
Saraswati Saud <saraswati.saud@gmail.com>
2017-12-08 08:12:56 +0545

    Events and Uni Directional Data Flow

--- src/Test.js
+++ src/Test.js
@@ -5,58 +5,39 @@ class Test extends React.Component {
   constructor() {
     super();
     this.state = {
+      value: 1,
       color: true,
       text: 'Click to change color'
     }
   }
-
-  handleClick = () => {
-    this.setState({
-      color: !this.state.color,
-      text: !this.state.color == true ? 'Click to get Green' : 'Click to get Red'
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <p className={this.state.color ? 'red' : 'green'}>Change the text color</p>
-        <button onClick={this.handleClick}>{this.state.text}</button>
-        <hr />
-        <Increment />
-      </div>
-    );
-  }
-}
-
-class Increment extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      value: 1
-    }
-  }
-
   increment = () => {
     this.setState({
       value: this.state.value + 1
     })
   }
-
   decrement = () => {
     this.setState({
       value: this.state.value - 1
     })
   }
+  handleClick = () => {
+    this.setState({
+      color: !this.state.color,
+      text: this.state.color === true ? 'Click green' : 'Click red'
+    })
+  }
 
   render() {
-    return(
-      <div>
-        <button onClick={this.increment}>Increment</button>
-        <p>{this.state.value}</p>
-        <button onClick={this.decrement}>Decrement</button>
-      </div>
-    )
+      return(
+        <div>
+          <button onClick={this.increment}>Increment</button>
+          <p>{this.state.value}</p>
+          <button onClick={this.decrement}>Decrement</button>
+          <hr />
+          <p className={this.state.color ? 'red' : 'green'}>Change the color</p>
+          <button onClick={this.handleClick}>{this.state.text}</button>
+        </div>
+      )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,47 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Test from './Test';
+import './index.css';
 
 class TodoList extends React.Component {
   constructor() {
     super();
+    this.changeStatus = this.changeStatus.bind(this);
     this.state = {
-      tasks: [{
-        name: 'Buy milk',
-        completed: false
-      },
-      {
-        name: 'Buy Bread',
-        completed: false
-      },
-      {
-        name: 'Buy Cheese',
-        completed: false
-      }]
+      tasks: [
+        {
+          name: 'Pintoo',
+          completed: false
+        },
+        {
+          name: 'Chintoo',
+          completed: false
+        },
+        {
+          name: 'Mintoo',
+          completed: false
+        }
+      ]
     }
+  }
+
+  changeStatus(index) {
+    var tasks = this.state.tasks;
+    var task = tasks[index];
+    task.completed = !task.completed;
+    this.setState({
+      tasks: tasks
+    })
   }
 
   render() {
     return(
       <div>
+        <p>Todo lists</p>
         <ul>
-          {
-            this.state.tasks.map(function(task) {
-              return <ListItems key={task.name} details={task.name} />
-            })
-          }
+          {this.state.tasks.map((task, index) => {
+            return <ListItem key={task.name}
+                             clickHandler={this.changeStatus}
+                             index = {index}
+                             details={task}
+                    />
+          })}
         </ul>
-        <Test />
       </div>
-    );
+    )
   }
 }
 
-class ListItems extends React.Component {
+class ListItem extends React.Component {
   render() {
     return(
-      <li>{this.props.details}</li>
-    );
+      <li onClick={ () => {
+        this.props.clickHandler(this.props.index);
+      }}
+        className={this.props.details.completed ? 'completed' : ''}>
+        {this.props.details.name}
+      </li>
+    )
   }
 }
 
